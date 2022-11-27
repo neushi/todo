24f3b155dea341ae46fcbcac82e8586b64710241
neushi <80911228+neushi@users.noreply.github.com>
2022-11-28 01:22:29 +0900

    backup

--- lib/workPool.rb
+++ lib/workPool.rb
@@ -2,40 +2,51 @@ require_relative '../lib/work'
 require "ruby-graphviz"
 
 class WorkPool
-    attr_reader :pool
-    # IDのダブりがないこと、
-    # 不在のIDを指していないこと
+  attr_reader :pool
+  # IDのダブりがないこと、
+  # 不在のIDを指していないこと
 
-    def initialize
-        @next_key = 1
-        @pool = Hash.new
+  def initialize
+    @next_key = 1
+    @pool = Hash.new
+  end
+
+  def add(work)
+    @pool[@next_key] = work
+    work.id = @next_key
+    @next_key += 1
+  end
+
+  def remove(work)
+  end
+
+  def top_works
+  end
+
+  def show_sub_works(work)
+  end
+
+  def part_of(id, id_sub)
+  end
+
+  def leafs
+  end
+
+
+  def export(out_file_name)
+    nodes = Hash.new
+    g = GraphViz.new( :G, :type => :digraph )
+    @pool.each_key do |key|
+      nodes[@pool[key].id] = g.add_nodes(@pool[key].name)
     end
-
-    def add(work)
-        @pool[@next_key] = work
-        work.id = @next_key
-        @next_key += 1
+    @pool.each_key do |key|
+      to_node = nodes[@pool[key].id]
+      from_nodes = @pool[key].sub_works
+      from_nodes.each do |fn|
+        g.add_edges(fn, to_node)
+      end
     end
-
-    def remove(work)
-    end
-
-    def top_works
-    end
-
-    def show_sub_works(work)
-    end
-
-    def part_of(id, id_sub)
-    end
-
-    def leafs
-    end
-
-
-    def export
-        gv = GraphViz.new( :G, :type => :digraph )
-        @pool.each_key do |key|
-        end
-    end
+    g.add_edges( nodes[1], nodes[2] )
+    g.output( :png => (out_file_name + ".png" ))
+  end
 end
--- test/work_test.rb
+++ test/work_test.rb
@@ -1,20 +1,68 @@
 require 'minitest/autorun'
 require_relative '../lib/work'
 
-class WorkTest < Minitest::Test     # Testで始まるか、終わるクラス名
-    # workを作れるか
-    # 作った直後のidは０
-    # 名前を変更できるか
-    def test_new                    # testで始まるメソッド名
-        w = Work.new("namae") 
-        assert_instance_of(Work, w)
-        assert_equal(0, w.id)
-        assert_equal("namae", w.name)
-        w.name = "name"
-        assert_equal("name", w.name)
-    end
-    # for_whatに0が入っていたら例外
-    # for_whatのリストがダブっていたら例外
-    # 
+class WorkTest < Minitest::Test   # Testで始まるか、終わるクラス名
+  # workを作れるか
+  def test_new          # testで始まるメソッド名
+    w = Work.new("namae") 
+    assert_instance_of(Work, w)
+    assert_equal(0, w.id)
+    assert_equal("namae", w.name)
+  end
+
+  # id,名前を変更できるか
+  def test_name
+    w = Work.new("namae") 
+    assert_equal("namae", w.name)
+    w.name = "name"
+    assert_equal("name", w.name)
+    assert_equal(0, w.id)
+    w.id = 345
+    assert_equal(345, w.id)
+  end
+
+# @date_create = Date.today
+# @date_modified = Date.today
+# @for_works = []
+# @sub_works = []
+# @cond_daylight = false
+# @cond_weekday = false
+  def test_date_relations_conditions
+    w = Work.new("namae") 
+    assert_equal(Date.today, w.date_created)
+    assert_equal(Date.today, w.date_modified)
+    w.date_created = Date.today - 1 
+    w.date_modified = Date.today - 1 
+    assert_equal(Date.today - 1, w.date_created)
+    assert_equal(Date.today - 1, w.date_modified)
+    w.for_works = [2,3]
+    w.sub_works = [12,33]
+    assert(w.for_works.eql?([2,3]))
+    assert(w.sub_works.eql?([12,33]))
+    refute(w.cond_daylight)
+    refute(w.cond_weekday)
+    w.cond_daylight = true
+    w.cond_weekday = true
+    assert(w.cond_daylight)
+    assert(w.cond_weekday)
+    w.cond_daylight = false
+    w.cond_weekday = false
+    refute(w.cond_daylight)
+    refute(w.cond_weekday)
+  end
+
+# for_whatのリストがダブっていたら例外 とか
+  def test_check(w)
+      assert_instance_of(Work, w.class)
+      assert_instance_of(Integer, w.id.class)
+      refute(0, @name.length)
+      assert_instance_of(Date, @date_created)
+      assert_instance_of(Date, @date_modified)
+      ダブりはとる、ぜんぶ数字でなければだめ
+      fail @for_works = []
+      fail @sub_works = []
+      assert_instance_of(Boolean, @cond_daylight)
+      assert_instance_of(Boolean, @cond_weekday)
+  end
 
 end
